18c0d06abf233736cf06b777fa4681937afe93aa
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-09-27 18:31:31 +1000

    Add success cli case

--- __tests__/cli.js
+++ __tests__/cli.js
@@ -2,12 +2,28 @@
 const {promisify} = require('util');
 const {join} = require('path');
 const exec = promisify(require('child_process').exec);
+const bin = join(__dirname, '../bin/run-when');
 
 describe('CLI mode', () => {
-  test('should run command if glob files matches', async () => {
-    const bin = join(__dirname, '../bin/run-when');
-    const result = await exec(`${bin} '["foo"]' 'echo bar'`);
+  const run = command => exec(`${bin} ${command}`);
 
-    console.log(result);
+  const modifyFixtures = () => {
+    const catPath = join(__dirname, '../__fixtures__/cat');
+
+    return exec(`echo miau > ${catPath}`);
+  };
+
+  const restoreFixtures = () => {
+    const catPath = join(__dirname, '../__fixtures__/cat');
+
+    return exec(`echo ":(" > ${catPath}`);
+  };
+
+  test('should run command if glob files matches', async () => {
+    await modifyFixtures();
+    const {stdout} = await run(`'["__fixtures__/**"]' 'echo fixtures changed'`);
+
+    expect(stdout.trim()).toEqual('fixtures changed');
+    await restoreFixtures();
   });
 });
